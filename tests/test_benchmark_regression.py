c254ef83a82250eae9924cc473278b861dd9fd59
Evgeny <rage19671999@gmail.com>
2025-07-20 21:42:19 +0300

    Update test_benchmark_regression.py

--- tests/test_benchmark_regression.py
+++ tests/test_benchmark_regression.py
@@ -31,6 +31,7 @@ async def bench_store():
 
 
 @pytest.mark.perf
+@pytest.mark.benchmark
 def test_semantic_search_speed(mem_benchmark, bench_store):
     loop = asyncio.get_event_loop()
     mem_benchmark(lambda: loop.run_until_complete(
